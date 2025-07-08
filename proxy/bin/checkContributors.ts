484a6159484f0913d473f830ed9e19034814e882
Erik Beuschau <bager.erik@gmail.com>
2025-07-08 09:58:27 +0200

    Add test step to verify contributors

---
+++ proxy/bin/checkContributors.ts
@@ -0,0 +1,46 @@
+import { readFileSync } from 'fs'
+import { exec } from 'node:child_process'
+import { exit } from 'node:process'
+import { promisify } from 'node:util'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const resolvePath = (...segments: string[]) =>
+  path.resolve(__dirname, ...segments)
+
+const execPromise = promisify(exec)
+
+export default async function checkContributors() {
+  const declaredContributors = readFileSync(
+    resolvePath('..', 'contributors.json'),
+    'utf-8',
+  )
+  const contributors = JSON.parse(declaredContributors) as Array<{
+    email: string
+    username: string
+  }>
+  const contributorEmails = new Set(contributors.map((c) => c.email))
+  const { stdout: allContributors, stderr } = await execPromise(
+    'git log --format="%ae" | sort | uniq',
+  )
+  if (stderr) {
+    // eslint-disable-next-line no-console
+    console.error('Error fetching contributors:', stderr)
+    exit(1)
+    return
+  }
+  allContributors.split('\n').forEach((email) => {
+    if (email.length > 0 && !contributorEmails.has(email)) {
+      // eslint-disable-next-line no-console
+      console.error(
+        `Contributor with email ${email} is not declared in contributors.json`,
+      )
+      exit(1)
+    }
+  })
+}
+
+void checkContributors()
