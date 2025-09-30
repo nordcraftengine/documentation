ed8e914953715c46cf58d4242f4eba15e26023f9
Erik Beuschau <bager.erik@gmail.com>
2025-09-30 08:34:53 +0200

    Fix tests 4 realz

--- proxy/src/utils/helpers.test.ts
+++ proxy/src/utils/helpers.test.ts
@@ -1,26 +1,22 @@
-import { describe, expect, it, test } from 'bun:test'
+import { describe, expect, test } from 'bun:test'
 import { chunk, kebabCase } from './helpers'
 
 describe('kebabCase', () => {
-  test('kebabCase', () => {
-    it('should convert a string to kebab case', () => {
-      expect(kebabCase('Hello World')).toBe('hello-world')
-      expect(kebabCase('foo_bar')).toBe('foo-bar')
-      expect(kebabCase('')).toBe('')
-      expect(kebabCase('base64')).toBe('base64')
-      expect(kebabCase('hello world _/# with special:chars')).toBe(
-        'hello-world-with-special-chars',
-      )
-    })
+  test('should convert a string to kebab case', () => {
+    expect(kebabCase('Hello World')).toBe('hello-world')
+    expect(kebabCase('foo_bar')).toBe('foo-bar')
+    expect(kebabCase('')).toBe('')
+    expect(kebabCase('base64')).toBe('base64')
+    expect(kebabCase('hello world _/# with special:chars')).toBe(
+      'hello-world-with-special-chars',
+    )
   })
 })
 
 describe('chunk', () => {
-  test('chunk', () => {
-    it('should chunk an array into smaller arrays of a specified size', () => {
-      expect(chunk([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]])
-      expect(chunk([1, 2, 3], 1)).toEqual([[1], [2], [3]])
-      expect(chunk([], 2)).toEqual([])
-    })
+  test('should chunk an array into smaller arrays of a specified size', () => {
+    expect(chunk([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]])
+    expect(chunk([1, 2, 3], 1)).toEqual([[1], [2], [3]])
+    expect(chunk([], 2)).toEqual([])
   })
 })
