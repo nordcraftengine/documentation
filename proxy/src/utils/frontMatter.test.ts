92d261f120ad2b6a375701e69467bfd5675041a2
Erik Beuschau <bager.erik@gmail.com>
2025-09-30 08:26:36 +0200

    Include more content in search index `codespan` content would not be included in the search index as it did not have any tokens. Now, when a token doesn't have any tokens, we add its `text` property to the search index. After adjusting how we extract text from tokens, I had to adjust how we get the `firstTextToken` as it would break if the token was `null` I also fixed 2 tests

--- proxy/src/utils/frontMatter.test.ts
+++ proxy/src/utils/frontMatter.test.ts
@@ -1,36 +1,38 @@
-import { expect, it, test } from 'bun:test'
+import { describe, expect, it, test } from 'bun:test'
 import { getFrontMatter } from './frontMatter'
 
-test('getFrontMatter', () => {
-  it('should extract front matter and body from text', () => {
-    const { attributes, body } = getFrontMatter(`
-  ---
-  title: Test title
-  description: Test: description
-  ---
+describe('getFrontMatter', () => {
+  test('getFrontMatter', () => {
+    it('should extract front matter and body from text', () => {
+      const { attributes, body } = getFrontMatter(`
+    ---
+    title: Test title
+    description: Test: description
+    ---
 
-  # The body
+    # The body
 
-  The body
-      `)
-    expect(attributes).toEqual({
-      title: 'Test title',
-      description: 'Test: description',
+    The body
+        `)
+      expect(attributes).toEqual({
+        title: 'Test title',
+        description: 'Test: description',
+      })
+      expect(body).toBe('# The body\n\nThe body')
     })
-    expect(body).toBe('# The body\n\nThe body')
-  })
-  it('should support no front matter', () => {
-    const { attributes, body } = getFrontMatter(`
-  # The body
+    it('should support no front matter', () => {
+      const { attributes, body } = getFrontMatter(`
+    # The body
 
-  The body
-      `)
-    expect(attributes).toEqual({})
-    expect(body).toBe('# The body\n\nThe body')
-  })
-  it('should support an empty string', () => {
-    const { attributes, body } = getFrontMatter(``)
-    expect(attributes).toEqual({})
-    expect(body).toBe('')
+    The body
+        `)
+      expect(attributes).toEqual({})
+      expect(body).toBe('# The body\n\nThe body')
+    })
+    it('should support an empty string', () => {
+      const { attributes, body } = getFrontMatter(``)
+      expect(attributes).toEqual({})
+      expect(body).toBe('')
+    })
   })
 })
