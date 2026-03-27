3109cc7f00e5d0230f1d6116449ad646b05ed19f
Erik Beuschau <bager.erik@gmail.com>
2026-03-27 11:28:19 +0100

    Move all md files to /static/raw

--- proxy/bin/buildAssets.ts
+++ proxy/bin/buildAssets.ts
@@ -28,14 +28,21 @@ const actionReferenceContent = fs.readFileSync(actionReferencePath, 'utf-8')
 const newActionContent = await includeActions(actionReferenceContent)
 fs.writeFileSync(actionReferencePath, newActionContent, 'utf-8')
 
-// Also copy over all markdown files + folders to /static
-fs.cpSync('../docs', './static', {
+// Also copy over all markdown files + folders to /static/raw
+if (fs.existsSync('./static')) {
+  fs.rmdirSync('./static', { recursive: true }) // Clean up old static folder if it exists
+}
+fs.mkdirSync('./static/raw', { recursive: true }) // Create new static/raw folder
+fs.cpSync('../docs', './static/raw', {
   recursive: true,
   filter: (src) => !src.endsWith('.webp') && !src.endsWith('.json'),
 })
 // and copy over the generated formula/action reference files
-fs.copyFileSync(formulaReferencePath, './static/references/formulas/index.md')
-fs.copyFileSync(actionReferencePath, './static/references/actions/index.md')
+fs.copyFileSync(
+  formulaReferencePath,
+  './static/raw/references/formulas/index.md',
+)
+fs.copyFileSync(actionReferencePath, './static/raw/references/actions/index.md')
 
 const structure = getStructure()
 // Create menu items structure
